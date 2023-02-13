a80cb3f403bb3b6b30174357e6bdb2f417dc0beb
Mansur <boronovmansur21@gmail.com>
2023-02-13 21:04:07 +0500

    yangi faylim
    dars1 faylini yukladim

---
+++ dars1.py
@@ -0,0 +1,8 @@
+yosh=int(input('yoshingiz nechada? '))
+if yosh<=4:
+    print('sizga krish bepul')
+elif yosh<=12:
+     print("sizga kirish 500 som")
+else:
+    print("sizga kirish 10000som") 
+    
